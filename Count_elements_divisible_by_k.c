4d301d5749fec789bf4c317808a4ca7a338a5414
Farid7786 <99642875+Farid7786@users.noreply.github.com>
2022-06-30 10:38:47 +0530

    #include<stdio.h> int main() {     int n,i,r,c=0,k;     scanf("%d%d",&n,&k);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%k==0)         {             c+=1;         }     }     printf("%d",c);     return 0; }

--- Count_elements_divisible_by_k.c
+++ Count_elements_divisible_by_k.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,k,c=0;
+    int n,i,r,c=0,k;
     scanf("%d%d",&n,&k);
     int arr[n];
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
         if(arr[i]%k==0)
         {
-            c++;
+            c+=1;
         }
     }
     printf("%d",c);
+    return 0;
 }
