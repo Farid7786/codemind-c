ad81a663294796d684d63f34433afe166a987e04
Farid7786 <99642875+Farid7786@users.noreply.github.com>
2022-06-30 15:28:29 +0530

    #include<stdio.h> int main() {     int n,i,k,sum=0,c=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }    for(i=0;i<n;i++)    {        if(arr[i]%2==0)        {            sum=sum+arr[i];        }        else        {            break;        }    }    printf("%d",sum); }

--- sum_of_array__elements_upto__first_odd_number.c
+++ sum_of_array__elements_upto__first_odd_number.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,sum=0;
+    int n,i,k,sum=0,c=0;
     scanf("%d",&n);
     int arr[n];
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    for(i=0;i<n;i++)
-    {
-        if(arr[i]%2!=0)
-        {
-            break;
-        }
-        else
-        {
-            sum=sum+arr[i];
-        }
-    }
-    printf("%d",sum);
+   for(i=0;i<n;i++)
+   {
+       if(arr[i]%2==0)
+       {
+           sum=sum+arr[i];
+       }
+       else
+       {
+           break;
+       }
+   }
+   printf("%d",sum);
 }
