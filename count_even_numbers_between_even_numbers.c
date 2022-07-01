6dd23875edcebc7bcb36d6d3084cbf2c4293923d
Farid7786 <99642875+Farid7786@users.noreply.github.com>
2022-07-01 20:56:42 +0530

    #include<stdio.h> int main() {     int n,i,c=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=1;i<n;i+=2)     {         if(arr[i]%2==0)         {             c++;         }     }     printf("%d",c); }

--- count_even_numbers_between_even_numbers.c
+++ count_even_numbers_between_even_numbers.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,c=0,r;
+    int n,i,c=0;
     scanf("%d",&n);
     int arr[n];
-    for(i=1;i<=n;i++)
+    for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    for(i=2;i<=n;i+=2)
+    for(i=1;i<n;i+=2)
     {
         if(arr[i]%2==0)
         {
