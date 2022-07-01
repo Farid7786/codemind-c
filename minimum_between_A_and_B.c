8f2dd121e1a29f0d27407ec612f5c4bcb88673e7
Farid7786 <99642875+Farid7786@users.noreply.github.com>
2022-07-01 21:27:42 +0530

    #include<stdio.h> int main() {     int n,i,a,b,sum=1000000,c=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if(arr[i]>=a&&arr[i]<=b)         {             if(sum>=arr[i])             {                 sum=arr[i];                 c++;             }         }     }     if(c==0)     {         printf("-1");     }     else     {         printf("%d",sum);     } }

---
+++ minimum_between_A_and_B.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a,b,sum=1000000,c=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>=a&&arr[i]<=b)
+        {
+            if(sum>=arr[i])
+            {
+                sum=arr[i];
+                c++;
+            }
+        }
+    }
+    if(c==0)
+    {
+        printf("-1");
+    }
+    else
+    {
+        printf("%d",sum);
+    }
+}
