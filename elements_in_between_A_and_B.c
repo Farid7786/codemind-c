480b7aa5977ffcc2f1cf2f9e47c31953b46627f2
Farid7786 <99642875+Farid7786@users.noreply.github.com>
2022-07-01 21:10:48 +0530

    #include<stdio.h> int main() {     int n,i,a,b,sum=0,c=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if(arr[i]>=a&&arr[i]<=b)         {             printf("%d ",arr[i]);             c++;         }     }     if(c==0)     {         printf("-1");     } }

---
+++ elements_in_between_A_and_B.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a,b,sum=0,c=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>=a&&arr[i]<=b)
+        {
+            printf("%d ",arr[i]);
+            c++;
+        }
+    }
+    if(c==0)
+    {
+        printf("-1");
+    }
+}
