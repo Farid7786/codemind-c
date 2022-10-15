14b12c92f4ddce8ce1487aaf16ebb37d50dc9009
ShaikFarid <99642875+Farid7786@users.noreply.github.com>
2022-10-15 13:47:17 +0530

    #include<stdio.h> int main() {     int n,a[100],i,c=0,sum=0,j,m=0;     float avg;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=1;j<=a[i];j++)         {             if(a[i]%j==0)             {                 c++;             }         }         if(c==2)         {             sum=sum+a[i];             m++;         }     }     avg=float(sum)/float(m);     printf("%.2f",avg);     return 0; }

--- Average_of_primes.c
+++ Average_of_primes.c
@@ -1,30 +1,30 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,c=0,j,m=0,sum=0;
-    scanf("%d",&n);
+    int n,a[100],i,c=0,sum=0,j,m=0;
     float avg;
-    int arr[n];
+    scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d",&a[i]);
     }
     for(i=0;i<n;i++)
     {
         c=0;
-        for(j=1;j<=arr[i];j++)
+        for(j=1;j<=a[i];j++)
         {
-            if(arr[i]%j==0)
+            if(a[i]%j==0)
             {
                 c++;
             }
         }
         if(c==2)
         {
-            sum=sum+arr[i];
+            sum=sum+a[i];
             m++;
         }
     }
     avg=float(sum)/float(m);
     printf("%.2f",avg);
+    return 0;
 }
