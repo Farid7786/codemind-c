89720cbaad6081c7a38358e146f2017699f4ba66
ShaikFarid <99642875+Farid7786@users.noreply.github.com>
2022-12-03 21:11:56 +0530

    #include<stdio.h> void wave(int *arr,int n) {     int i,c=0,d=0;     for(i=0;i<n-2;i+=2)     {         if((arr[i]<arr[i+1])&&(arr[i+2]<arr[i+1]))         {             c++;         }         else if(arr[i]>arr[i+1]||(arr[i+2]>arr[i+1]))         {             c=0;             break;         }     }     if(c!=0)     {         printf("%d",c);     }     else     {         printf("-1");     } } int main() {     int n,i;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     wave(arr,n); }

---
+++ wave_count.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void wave(int *arr,int n)
+{
+    int i,c=0,d=0;
+    for(i=0;i<n-2;i+=2)
+    {
+        if((arr[i]<arr[i+1])&&(arr[i+2]<arr[i+1]))
+        {
+            c++;
+        }
+        else if(arr[i]>arr[i+1]||(arr[i+2]>arr[i+1]))
+        {
+            c=0;
+            break;
+        }
+    }
+    if(c!=0)
+    {
+        printf("%d",c);
+    }
+    else
+    {
+        printf("-1");
+    }
+}
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    wave(arr,n);
+}
